6cdc9271863f54418e149e2453df0808ef81a7fb
NiDolIx <nikita_dolgih@mail.ru>
2024-03-11 19:17:17 +0300

    Update UserRepository

--- src/main/java/com/example/restaurant/repository/UserRepository.java
+++ src/main/java/com/example/restaurant/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.restaurant.repository;
 
+import com.example.restaurant.dto.UserDto;
 import com.example.restaurant.model.User;
+import com.example.restaurant.repository.mapper.UserDtoMapper;
 import com.example.restaurant.repository.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -17,6 +19,41 @@ public class UserRepository {
     @Autowired
     public UserRepository(NamedParameterJdbcTemplate template) {
         this.template = template;
+    }
+
+    public void updateUser(UserDto userDto, Long userId) {
+        String sql = """
+                UPDATE public.user
+                SET
+                    user_name = :user_name,
+                    user_surname = :user_surname,
+                    user_lastname = :user_lastname,
+                    user_phone = :user_phone
+                WHERE user_id = :user_id
+                """;
+        SqlParameterSource parameters = new MapSqlParameterSource()
+                .addValue("user_name", userDto.getUserName())
+                .addValue("user_surname", userDto.getUserSurname())
+                .addValue("user_lastname", userDto.getUserLastname())
+                .addValue("user_phone", userDto.getUserPhoneNumber())
+                .addValue("user_id", userId);
+        template.update(sql, parameters);
+    }
+
+    public Optional<UserDto> findUserById(Long userId) {
+        String sql = """
+                SELECT
+                    user_name, 
+                    user_surname, 
+                    user_lastname, 
+                    user_phone
+                FROM public.user
+                WHERE user_id = ?
+                """;
+        return template.getJdbcTemplate()
+                .query(sql, new UserDtoMapper(), userId)
+                .stream()
+                .findAny();
     }
 
     public Optional<User> findUserByPhoneNumber(String phoneNumber) {
