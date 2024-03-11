ba9b209e0cd6a6bd597e3382e67612ee23260513
NiDolIx <nikita_dolgih@mail.ru>
2024-03-11 19:12:06 +0300

    Update security config

--- src/main/java/com/example/restaurant/config/WebSecurityConfig.java
+++ src/main/java/com/example/restaurant/config/WebSecurityConfig.java
@@ -1,12 +1,18 @@
 package com.example.restaurant.config;
 
 
+import com.example.restaurant.repository.UserRepository;
+import com.example.restaurant.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +21,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfig {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public WebSecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,6 +44,19 @@ public class WebSecurityConfig {
                         .logoutSuccessUrl("/login")
                         .deleteCookies())
                 .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserService(userRepository, passwordEncoder());
     }
 
     @Bean
