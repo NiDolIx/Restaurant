a0e8348235faaf72ba07796418f1a5d32dd800a0
NiDolIx <nikita_dolgih@mail.ru>
2024-03-11 19:09:26 +0300

    Create custom UserDetails

---
+++ src/main/java/com/example/restaurant/config/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.example.restaurant.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private Long userId;
+    private String userPhoneNumber;
+    private String userPassword;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return userPhoneNumber;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
